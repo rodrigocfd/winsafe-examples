e5cb51ecb11ad8d413274ca270271da1e844449e
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-06-29 11:40:32 -0300

    Uniform name for resources.res.

--- 01_button_click/build.rs
+++ 01_button_click/build.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-	const RES: &str = "resources/example01.res";
+	const RES: &str = "resources/resources.res";
 
 	// Create a copy of .res file, and append .lib extension.
 	// This is necessary because of a limitation in the toolchain.
